0a246dfd97b022fab3b293c9fa850660febe2ff8
hiro <kiryuinh32@gmail.com>
2021-08-26 15:57:52 +0900

    feat: requestLine 에서 method 와 url 추출

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -6,7 +6,10 @@ import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.Map;
 
+import controller.AbstractController;
+import controller.CreateUserController;
 import db.DataBase;
+import model.HttpMethod;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,6 +20,12 @@ import static util.StringUtils.*;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+    private static final Map<String, AbstractController> controllerMap;
+
+    static {
+        controllerMap = new HashMap<>();
+        controllerMap.put("/create", new CreateUserController());
+    }
 
     private Socket connection;
     private DataBase dataBase;
@@ -33,24 +42,36 @@ public class RequestHandler extends Thread {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
-            DataOutputStream dos = new DataOutputStream(out);
             String line = bufferedReader.readLine();
-            String url = extractPath(line);
+            String[] requestLine = split(line," ");
+            HttpMethod method = HttpMethod.valueOf(requestLine[0]);
+            String url = requestLine[1];
+            AbstractController controller = controllerMap.get(url);
+            controller.service(method);
+
+            //FIXME
             Map<String, String> header = new HashMap<>();
-            if ("POST".equals(extractHttpMethod(line))) {
-                while (!(line = bufferedReader.readLine()).equals("")) {
+            DataOutputStream dos = new DataOutputStream(out);
+            if ("POST".equals(requestLine)) {//Request Line에서 메서드 추출
+                while (!(line = bufferedReader.readLine()).equals("")) {//http 메세지의 header를 map에 담는다.
                     log.debug("line : {}", line);
                     String[] splitedLine = line.split(": ");
 
-                    header.put(splitedLine[0], splitedLine[1]);
+                    header.put(splitedLine[0], splitedLine[1]);//Ket = content-length ,  value = 길이 를 담는다
                 }
-                String body = IOUtils.readData(bufferedReader,Integer.parseInt(header.get("Content-Length")));
-                User newUser = User.of(parseQueryString(body));
-                dataBase.addUser(newUser);
+
+                String body = IOUtils.readData(bufferedReader,Integer.parseInt(header.get("Content-Length")));//길이만큼 유저정보 읽어서 body에 담는다
+                User newUser = User.of(parseQueryString(body));//담은 body로 유저 생성
+                DataBase.addUser(newUser);//넣고
                 log.debug("newUser : {}", newUser);
-                response302(dos);
+                response302(dos);//응답
                 return;
             }
+
+        // String RquestLine; -> /create, /login 분리 & httomethod 분리 -> 해당하는 controller 호출(HttoMethod) ->  각 컨트롤러가 가진 response메서드 사용
+        //  GET/POST/PUT 분리 -> POST/creat ->
+
+
             File file = new File(PARENT_DIRECTORY + url);
             byte[] body = Files.readAllBytes(file.toPath());
             response200Header(dos, body.length);
