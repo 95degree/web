ae48c54fb90e07e31fec335bc8aa574ded9f5636
hiro <kiryuinh32@gmail.com>
2021-08-26 15:53:42 +0900

    refactor: split 메서드 추상화

--- src/main/java/util/StringUtils.java
+++ src/main/java/util/StringUtils.java
@@ -1,23 +1,9 @@
 package util;
 
-import java.util.Map;
-
 public class StringUtils {
-    public static final int REQUEST_LINE_PATH = 1;
     public static final String PARENT_DIRECTORY = "./webapp";
 
-    public static String extractHttpMethod(String requestLine) {
-        String[] split = requestLine.split(" ");
-        return split[0];
-    }
-
-    public static String extractPath(String requestLine) {
-        String[] split = requestLine.split(" ");
-        return split[REQUEST_LINE_PATH];
-    }
-
-    public static String extractQuery(String requestLine) {
-        String[] split = requestLine.split("\\?");
-        return split[REQUEST_LINE_PATH];
+    public static String[] split(String string, String separator) {
+        return string.split(separator);
     }
 }
