b228da650ab4b688bfd8c2c9f6387104d2befd64
hiro <kiryuinh32@gmail.com>
2021-09-02 15:47:07 +0900

    refactor: 사용하지 않는 Database 멤버 삭제

--- src/main/java/controller/AbstractController.java
+++ src/main/java/controller/AbstractController.java
@@ -1,22 +1,35 @@
 package controller;
 
+
 import model.HttpMethod;
+import model.HttpRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
 import static model.HttpMethod.*;
 
 public abstract class AbstractController implements HttpController {
 
+    private static final Logger log = LoggerFactory.getLogger(AbstractController.class);
+
     @Override
-    public void service(HttpMethod httpMethod) {
+        public void service(HttpRequest httpRequest, BufferedReader bufferedReader, DataOutputStream dos) throws IOException {
+        HttpMethod httpMethod = httpRequest.getHttpMethod();
         if (httpMethod == POST) {
-            doPost();
+            doPost(httpRequest, bufferedReader, dos);
         } else if (httpMethod == GET) {
-            doGet();
+            doGet(httpRequest, bufferedReader, dos);
         }
     }
 
     public void doPost(HttpRequest httpRequest, BufferedReader bufferedReader, DataOutputStream dos) throws IOException{
     }
 
-    abstract void doGet();
+    public void doGet(HttpRequest httpRequest, BufferedReader bufferedReader, DataOutputStream dos) throws IOException{
+    }
 }
 
--- src/main/java/controller/HttpController.java
+++ src/main/java/controller/HttpController.java
@@ -2,6 +2,10 @@ package controller;
 
 import model.HttpRequest;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
 public interface HttpController {
-    void service(HttpMethod httpMethod);
+    void service(HttpRequest httpRequest, BufferedReader bufferedReader, DataOutputStream dos) throws IOException;
 }
