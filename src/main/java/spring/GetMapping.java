f4dc1a38178b645589710a4730ee731e5529e35b
95degree <95degree@naver.com>
2021-10-29 16:26:23 +0900

    feat : GetMapping 어노테이션 생성

--- src/main/java/spring/GetMapping.java
+++ src/main/java/spring/GetMapping.java
@@ -3,9 +3,12 @@ package spring;
 import model.HttpMethod;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
 @RequestMapping(method = HttpMethod.GET)
 public @interface GetMapping {
 
