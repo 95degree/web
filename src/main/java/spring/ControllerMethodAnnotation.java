d1fcf334cbf1bed8487f1e25d447830ea8d7e4e1
95degree <95degree@naver.com>
2021-10-29 16:33:55 +0900

    feat : ControllerMethodAnnotation 생성

---
+++ src/main/java/spring/ControllerMethodAnnotation.java
@@ -0,0 +1,25 @@
+package spring;
+
+import model.HttpMethod;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class ControllerMethodAnnotation {
+
+    private final Annotation annotation;
+
+    public ControllerMethodAnnotation(Annotation annotation) {
+        this.annotation = annotation;
+    }
+
+    public RequestInfo createRequestInfo() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Class<? extends Annotation> annotationType = annotation.annotationType();
+        RequestMapping requestMapping = annotationType.getAnnotation(RequestMapping.class);
+        Method valueMethod = annotationType.getMethod("value");
+        String url = (String) valueMethod.invoke(annotation);
+        HttpMethod httpMethod = requestMapping.method();
+        return new RequestInfo(url, httpMethod);
+    }
+}
