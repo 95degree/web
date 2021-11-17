ff9a30b3e540c5f2c63d77f6fccbc6e4916c4d17
95degree <95degree@naver.com>
2021-11-17 17:53:27 +0900

    feat : Body 클래스 생성 - body에 들어온 문자열을 Map으로 파싱해서 가지고 있는 클래스

---
+++ src/main/java/model/Body.java
@@ -0,0 +1,18 @@
+package model;
+
+import java.util.Map;
+
+import static util.HttpRequestUtils.parseQueryString;
+
+public class Body {
+
+    private Map<String, String> body;
+
+    private Body(Map<String, String> body) {
+        this.body = body;
+    }
+
+    public static Body from(String body) {
+        return new Body(parseQueryString(body));
+    }
+}
