a0be19cc6837a2674cd00cecec88962e62a13398
95degree <95degree@naver.com>
2021-11-17 17:51:30 +0900

    feat : 필요없는 클래스 삭제

--- src/main/java/controller/DefaultController.java
+++
@@ -1,8 +0,0 @@
-package controller;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class DefaultController extends AbstractController{
-    private static final Logger log = LoggerFactory.getLogger(DefaultController.class);
-}
